5e8ac5f4c4e72b641e8a29332b803096b95eaead
LAPTOP-BIEB081K\riots <rio.tskw.0704@gmail.com>
2021-12-02 12:20:35 +0900

    消音&自動でプレイヤーに切り替えないようにするのを追加。

--- Ver.camera/app1.js
+++ Ver.camera/app1.js
@@ -15,6 +15,8 @@ video.id       = 'video';
 video.width    = cameraSize.w;
 video.height   = cameraSize.h;
 video.autoplay = true;
+video.muted=true;/*消音にする*/
+video.playsInline=true;/*自動でプレイヤーに切り替えないようにする*/
 document.getElementById('videoPreview1').appendChild(video);
 
 
